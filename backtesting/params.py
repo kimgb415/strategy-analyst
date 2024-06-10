b02e575d89308407a577900f8b8f9988c5d295fa
gabhyun kim <kimgb415@gmail.com>
2024-06-10 21:20:07 +0900

    7% annual return with no selling

--- backtesting/params.py
+++ backtesting/params.py
@@ -1,7 +1,8 @@
 params_to_optimize = dict(
-    period=range(10, 51, 5),
-    stddev=[1, 2, 3],
-    stop_loss_pct=[0.01, 0.02, 0.03],
-    take_profit_pct=[0.01, 0.02, 0.03],
-    risk_pct=[0.01, 0.02, 0.03],
+    period=(20, 30, 40, 50),
+    devfactor=(1.5, 2.0, 2.5, 3.0),
+    stop_loss_pct=(0.02, 0.03, 0.04, 0.05),
+    take_profit_pct=(0.02, 0.03, 0.04, 0.05),
+    max_positions=(3, 4, 5, 6),
+    risk_pct=(0.01, 0.015, 0.02, 0.025),
 )
--- backtesting/strategy.py
+++ backtesting/strategy.py
@@ -1,40 +1,44 @@
 import backtrader as bt
 
 class MyStrategy(bt.Strategy):
-    params = (('period', 20), ('stddev', 2), ('stop_loss_pct', 0.02), ('take_profit_pct', 0.02), ('risk_pct', 0.02))
+    params = (('period', 50), ('devfactor', 2.0), ('stop_loss_pct', 0.03), ('take_profit_pct', 0.03), ('max_positions', 5), ('risk_pct', 0.015))
 
     def __init__(self):
-        self.data_close = self.datas[0].close
-        self.sma = bt.indicators.SimpleMovingAverage(self.data_close, period=self.params.period)
-        self.stddev = bt.indicators.StandardDeviation(self.data_close, period=self.params.period)
-        self.upper_band = self.sma + (self.stddev * self.params.stddev)
-        self.lower_band = self.sma - (self.stddev * self.params.stddev)
+        self.data.close = self.datas[0].close
+        self.sma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.period)
+        self.stddev = bt.indicators.StandardDeviation(self.data.close, period=self.params.period)
+        self.lower_band = self.sma - self.stddev * self.params.devfactor
+        self.upper_band = self.sma + self.stddev * self.params.devfactor
         self.order = None
-        self.entry_price = None
+        self.position_size = self.params.risk_pct * self.broker.get_value()
 
     def next(self):
         if self.order:
             return
 
         if not self.position:
-            if self.data_close[0] <= self.lower_band[0]:
-                self.order = self.buy(exectype=bt.Order.Market)
-                self.entry_price = self.data_close[0]
-            elif self.data_close[0] >= self.upper_band[0]:
-                self.order = self.sell(exectype=bt.Order.Market)
-                self.entry_price = self.data_close[0]
+            if self.data.close <= self.lower_band:
+                self.order = self.buy(exectype=bt.Order.Market, size=self.position_size)
+            elif self.data.close >= self.upper_band:
+                self.order = self.sell(exectype=bt.Order.Market, size=self.position_size)
         else:
             if self.position.size > 0:
-                if self.data_close[0] >= self.entry_price * (1 + self.params.take_profit_pct):
-                    self.close(exectype=bt.Order.Market)
-                elif self.data_close[0] <= self.entry_price * (1 - self.params.stop_loss_pct):
-                    self.close(exectype=bt.Order.Market)
+                if self.data.close >= self.upper_band:
+                    self.close()
+                elif self.data.close <= self.position.price * (1 - self.params.stop_loss_pct):
+                    self.close()
             elif self.position.size < 0:
-                if self.data_close[0] <= self.entry_price * (1 - self.params.take_profit_pct):
-                    self.close(exectype=bt.Order.Market)
-                elif self.data_close[0] >= self.entry_price * (1 + self.params.stop_loss_pct):
-                    self.close(exectype=bt.Order.Market)
+                if self.data.close <= self.lower_band:
+                    self.close()
+                elif self.data.close >= self.position.price * (1 + self.params.take_profit_pct):
+                    self.close()
 
-    def stop(self):
-        # No need to set anything in the stop method
-        pass
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Rejected, order.Canceled, order.Margin, order.Expired]:
+            self.order = None
+
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            pass
