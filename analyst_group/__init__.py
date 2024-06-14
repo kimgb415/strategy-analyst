7901bef448a2c603a06180a031016fd94175b2b3
gabhyun kim <kimgb415@gmail.com>
2024-06-14 16:23:51 +0900

    remove ollama comments

--- analyst_group/__init__.py
+++ analyst_group/__init__.py
@@ -1,5 +1,4 @@
 from langchain_nvidia_ai_endpoints import ChatNVIDIA as nvda
-# from langchain_community.chat_models import ChatLiteLLM as ChatNVIDIA
 from langchain.globals import set_llm_cache
 from langchain_community.cache import SQLAlchemyCache
 from sqlalchemy import create_engine
@@ -10,7 +9,6 @@ ChatNVIDIA = functools.partial(nvda, temperature=0.3)
 
 NVDA_MODEL = "meta/llama3-70b-instruct"
 # NVDA_MODEL = "meta/llama3-8b-instruct"
-# NVDA_MODEL = "ollama/llama3"
 
 
 engine = create_engine("sqlite:///cache.db")
