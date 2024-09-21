30b4182915d9a2907fac95c82836fd9ebff448b4
gabhyun kim <kimgb415@gmail.com>
2024-09-22 08:52:15 +0900

    gemma 27b 15% annual return result

--- backtesting/params.py
+++ backtesting/params.py
@@ -1,7 +1,3 @@
 params_to_optimize = dict(
-    bb_period=(10, 20, 30, 40),
-    bb_std_dev=(1.5, 2.0, 2.5, 3.0),
-    bb_width_low=(0.03, 0.05, 0.07, 0.09),
-    bb_width_high=(0.08, 0.10, 0.12, 0.15),
-    position_size=(0.01, 0.02, 0.03, 0.04),
+    period=(10, 20, 30, 40), 
 )
--- backtesting/strategy.py
+++ backtesting/strategy.py
@@ -1,40 +1,17 @@
 import backtrader as bt
 
 class MyStrategy(bt.Strategy):
-    params = (('bb_period', 20), ('bb_std_dev', 1.5), ('bb_width_low', 0.07), ('bb_width_high', 0.10), ('position_size', 0.04))
+    params = (
+        ('period', 20),  
+    )
 
     def __init__(self):
-        self.data_close = self.datas[0].close
-        self.data_high = self.datas[0].high
-        self.data_low = self.datas[0].low
-
-        self.bb = bt.indicators.BBands(self.data_close, period=self.p.bb_period, devfactor=self.p.bb_std_dev)
-        self.bbw = (self.bb.lines.top - self.bb.lines.bot) / self.bb.lines.mid
-
-        self.long_signal = bt.And(bt.ind.CrossDown(self.bbw, self.p.bb_width_low), bt.ind.CrossDown(self.data_close, self.bb.lines.bot))
-        self.short_signal = bt.And(bt.ind.CrossDown(self.bbw, self.p.bb_width_low), bt.ind.CrossUp(self.data_close, self.bb.lines.top))
-
-        self.long_exit = bt.Or(bt.ind.CrossUp(self.data_close, self.bb.lines.top), bt.ind.CrossUp(self.bbw, self.p.bb_width_high))
-        self.short_exit = bt.Or(bt.ind.CrossDown(self.data_close, self.bb.lines.bot), bt.ind.CrossUp(self.bbw, self.p.bb_width_high))
+        self.sma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.period)
 
     def next(self):
         if self.position.size == 0:
-            if self.long_signal[0]:
-                self.buy(size=self.p.position_size * self.broker.cash)
-            elif self.short_signal[0]:
-                self.sell(size=self.p.position_size * self.broker.cash)
-        elif self.position.size > 0:
-            if self.long_exit[0]:
-                self.close()
-        elif self.position.size < 0:
-            if self.short_exit[0]:
-                self.close()
-
-# Initialize the cerebro
-cerebro = bt.Cerebro()
-
-# Add the strategy
-cerebro.addstrategy(MyStrategy)
-
-# Run the backtest
-cerebro.run()
+            if self.data.close[0] > self.sma[0]:
+                self.buy()
+        else:
+            if self.data.close[0] < self.sma[0]:
+                self.sell()
